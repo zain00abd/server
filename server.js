75185e30565162fd8e19ff4c0b7710905030267f
zain00abd <zainabflash1998@gmail.com>
2024-05-12 22:12:51 +0300

    editserver

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors());
 const port = 3001 || process.env.PORT
 
-mongoose.connect("mongodb+srv://zaindiv:SK7A2fOZbLeJ08Ix@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://zaindiv:Zain007abd@cluster0.32r5dqe.mongodb.net/all-data?retryWrites=true&w=majority")
 
 app.get('/user', async (req, res)=>{
   const alluser = await data.find()
